783f4340bea2d81f008bd3f6151408a037700b6b
Westley <WestleyArgentum@gmail.com>
2013-11-20 19:41:21 -0800

    updated tests to use the new interface

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,21 +11,19 @@ end
 testmodel = TestThing({ "string_key" => 69 })
 
 # test symbol tree
-head = SymbolNode(:testmodel, nothing)
+record(:testmodel)
 
-child1 = SymbolNode(:member1, head)
-push!(head.children, child1)
+record(:testmodel, :member1)
 
-child2 = SymbolNode("string_key", child1)
-push!(child1.children, child2)
+record(:testmodel, :member1, "string_key")
 
 # -------
 
-@assert haskey(head, :testmodel)
-@assert haskey(head, (:testmodel, :member1))
-@assert haskey(head, (:testmodel, :member1, "string_key"))
+@assert hasrecord(:testmodel)
+@assert hasrecord(:testmodel, :member1)
+@assert hasrecord(:testmodel, :member1, "string_key")
 
-@assert !haskey(head, (:testmodel, :member1, "string_key", "does_not_exist"))
+@assert !hasrecord(:testmodel, :member1, "string_key", "does_not_exist")
 
-@assert getindex(head, :testmodel) != nothing
-@assert getindex(head, :testmodel, :member1, "string_key") == 69
+@assert getrecord(:testmodel) != nothing
+@assert getrecord(:testmodel, :member1, "string_key") == 69
