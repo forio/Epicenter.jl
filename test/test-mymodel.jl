dbd9feea610b5aced1c1499e716c705793a26190
Westley <WestleyArgentum@gmail.com>
2014-01-15 22:44:50 -0800

    fixed broken tests

--- test/test-mymodel.jl
+++ test/test-mymodel.jl
@@ -1,29 +1,30 @@
 
-require("MyModel.jl")
+
+require("$(dirname(@__FILE__))/MyModel.jl")
 using MyModel
 using Base.Test
 
 init()
 @test typeof(model) === MyModel.ModelType
-@test start_year === 2014
-@test curr_year === start_year
+@test MyModel.start_year === 2014
+@test curr_year === MyModel.start_year
 @test length(model.results_over_time) === 1
-@test fetch_records() != {}
+@test Unicorn.fetch_records() != {}
 
 advance()
-@test curr_year === 2015
+@test MyModel.curr_year === 2015
 @test length(model.results_over_time) === 2
 
 advance()
-@test curr_year === 2015
-@test length(model.results_over_time) === 2
+@test MyModel.curr_year === 2016
+@test length(model.results_over_time) === 3
 
 reset()
 @test typeof(model) === MyModel.ModelType
-@test start_year === 2014
-@test curr_year === start_year
+@test MyModel.start_year === 2014
+@test MyModel.curr_year === MyModel.start_year
 @test length(model.results_over_time) === 1
-@test take_records() != {}
+@test Unicorn.take_records() != {}
 
 advance()
-@test curr_year === 2015
+@test MyModel.curr_year === 2015
