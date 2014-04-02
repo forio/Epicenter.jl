b63df741dc9e161f3b0d7c9a880b0c950549a26c
Westley <WestleyArgentum@gmail.com>
2014-04-02 16:56:03 -0700

    tests to go along with  error stuff

--- test/test-persistence.jl
+++ test/test-persistence.jl
@@ -6,22 +6,35 @@ type TestThing
     member1
 end
 
-# test data
 testmodel = TestThing({ "string_key" => 69 })
 
-# test symbol tree
+# test basic record functionality -------
 record(:testmodel, :member1, "string_key")
-
 record(:testmodel, :member1)
-
 record(:testmodel)
-# -------
 
 @assert hasrecord(:testmodel)
 @assert hasrecord(:testmodel, :member1)
 @assert hasrecord(:testmodel, :member1, "string_key")
 
-#@assert !hasrecord(:testmodel, :member1, "string_key", "does_not_exist")
-
 @assert getrecord(:testmodel) != nothing
 @assert getrecord(:testmodel, :member1, "string_key") == 69
+# -------
+
+# test https://github.com/forio/Epicenter.jl/pull/9 -------
+function issue9(value, value_keys...)
+    some_local_variable = value
+
+    try
+        record(:some_local_variable, value_keys...)
+        throw("we shouldn't get here, record should have thrown an exception")
+
+    catch err
+        @assert err != "we shouldn't get here, record should have thrown an exception"
+    end
+end
+
+issue9(1234)
+issue9({"cow" => "moo"}, "cow")
+# -------
+
