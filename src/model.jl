5f073338b8913bcb49653701b32a44b99862c6d1
Westley <WestleyArgentum@gmail.com>
2014-05-22 16:25:56 -0700

    subscribe interface

--- src/model.jl
+++ src/model.jl
@@ -1,4 +1,16 @@
 
+_JULIET_EXISTS = try
+    import Juliet
+    true
+end
+
+function subscribe(event_name, fn_handler)
+    !_JULIET_EXISTS && return
+
+    event_sys = Main.Juliet.get_system(Main.Juliet.Event)
+    Main.Juliet.Event.register_handler(event_sys, "epicenter-$event_name", fn_handler)
+end
+
 # -------
 
 function setparam(params...)
@@ -6,6 +18,3 @@ function setparam(params...)
         eval(Main, :($(param[1]) = $(param[2])))
     end
 end
-
-# -------
-
